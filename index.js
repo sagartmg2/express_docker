b1d5930b8b69ea241c25cf31b14ea34df7429c9d
dev.sagartmg <dev.sagartmg@gmail.com>
2024-12-27 13:30:41 +0545

    docker for github
    with node_modules volume unbinded

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+var jwt = require('jsonwebtoken');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const bcrypt = require('bcrypt');
+
+app.get('/', async (req, res) => {
+  console.log("heree...");
+
+  try {
+
+    bcrypt.hash("myPlaintextPassword", 10).then(function (hash) {
+      console.log({ hash })
+    });
+
+    var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+    res.send('Hello World 1 2 3 4 5!' + token);
+  } catch (err) {
+    console.error('Error hashing password:', err);
+    res.status(500).send('Internal server error');
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app 1 2 listening at http://localhost:${port}`);
 });
--- z_steps.md
+++ z_steps.md
@@ -9,6 +9,24 @@ docker run --name <container_name>  <image_name>
 docker run -d -p 8000:8000 --name express_container express_image
 
 
-3.
+3. volumes setup.
+    docker run -d -p 8000:8000 -v $(pwd):/server --name express_container express_image
+
+    bind voulume except node moduels
 
 
+    docker run -d -p 8000:8000 -v $(pwd):/server -v /server/node_modules --name express_container express_image
+
+    
+
+
+
+
+
+> docker compose up : if there are old images and if Dockerfile has not been changed, then it uses caching and uses old 
+
+in that case,
+
+docker compose build --no-cache
+and then
+docker compose up
